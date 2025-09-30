4879a346f32afbfac3eef67ef5c5463f5cf1dba7
altivelis <ayupon89821026@gmail.com>
2025-09-30 22:35:30 +0900

    剥ぎ取り名人の発動条件を殴った時から殴ってダメージを与えた時　にナーフ

--- scripts/skills/lootMaster.js
+++ scripts/skills/lootMaster.js
@@ -1,14 +1,20 @@
 import * as mc from "@minecraft/server";
 import { getSkill } from "../main";
 
-mc.world.afterEvents.entityHitEntity.subscribe(data=>{
-  const attacker = data.damagingEntity;
-  const hurter = data.hitEntity;
-  if (!(attacker instanceof mc.Player) || !(hurter instanceof mc.Entity)) return; // プレイヤーとモブのみ対象
-  const skill = getSkill(attacker);
-  if (!skill || skill.id !== "loot_master") return; // スキルが剥ぎ取り名人でない場合は処理をスキップ
-  // モブがドロップするアイテムを取得
-  hurter.addTag("lootMaster"); // ドロップアイテムを取得するためのタグを追加
-  attacker.runCommand(`loot spawn ${hurter.location.x} ${hurter.location.y} ${hurter.location.z} kill @e[type=${hurter.typeId}, tag=lootMaster] mainhand`)
-  hurter.removeTag("lootMaster")
+mc.world.afterEvents.entityHitEntity.subscribe(data => {
+  const {damagingEntity, hitEntity} = data;
+  if (damagingEntity instanceof mc.Player === false) return;
+  if (!getSkill(damagingEntity) || getSkill(damagingEntity).id !== "loot_master") return;
+  let event = mc.world.afterEvents.entityHurt.subscribe(data2 => {
+    if (data2.hurtEntity.isValid == false) return;
+    if (data2.hurtEntity.id != hitEntity.id) return;
+    if (data2.damageSource.cause !== mc.EntityDamageCause.entityAttack) return;
+    if (data2.damageSource.damagingEntity.id != damagingEntity.id) return;
+    hitEntity.addTag("lootMaster"); // ドロップアイテムを取得するためのタグを追加
+    damagingEntity.runCommand(`loot spawn ${hitEntity.location.x} ${hitEntity.location.y} ${hitEntity.location.z} kill @e[type=${hitEntity.typeId}, tag=lootMaster] mainhand`)
+    hitEntity.removeTag("lootMaster");
+  })
+  mc.system.run(() => {
+    mc.world.afterEvents.entityHurt.unsubscribe(event);
+  })
 })
