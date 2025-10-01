8efdc48f305a00d6be9c67a40d20282732ced490
altivelis <ayupon89821026@gmail.com>
2025-10-01 21:33:26 +0900

    コード軽量化、パーティクル追加

--- scripts/skills/vampire.js
+++ scripts/skills/vampire.js
@@ -3,7 +3,14 @@ import { getSkill } from "../main";
 
 mc.world.afterEvents.entityHurt.subscribe(data=>{
   const player = data.damageSource.damagingEntity;
-  if(!player || !(player instanceof mc.Player) || !getSkill(player) || getSkill(player).id !== "vampire") return;
+  if(!player) return;
+  if(!(player instanceof mc.Player)) return;
+  let skill = getSkill(player);
+  if(!skill || skill.id !== "vampire") return;
   const healthComponent = player.getComponent(mc.EntityHealthComponent.componentId);
+  if(!healthComponent) return;
+  if(healthComponent.currentValue >= healthComponent.effectiveMax) return; // 満タンならスキップ
+  // ダメージ分回復する
   healthComponent.setCurrentValue(Math.min(Math.floor(healthComponent.currentValue + data.damage), healthComponent.effectiveMax));
+  player.dimension.spawnParticle("minecraft:heart_particle", player.location);
 })
